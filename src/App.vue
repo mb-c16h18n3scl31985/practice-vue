c20ef266c8583aa8712d34ed0d427142cff59399
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-06-21 17:35:24 +0900

    Add: Vueインスタンス生成時にストレージの値を自動的にfetch

--- src/App.vue
+++ src/App.vue
@@ -1,178 +1,185 @@
 <template>
-  <div class='app__main'>
-    <!-- 絞り込みラジオボタン -->
+    <div class='app__main'>
+        <!-- 絞り込みラジオボタン -->
 
-    <table>
-      <thead>
-      <tr>
-        <th class="id" scope="col">ID</th>
-        <th class="comment" scope="col">コメント</th>
-        <th class="state" scope="col">状態</th>
-        <th class="button" scope="col">-</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="item in todos" :key="item.id">
-        <th scope="row">{{ item.id }}</th>
-        <td>{{ item.comment }}</td>
-        <td class="state">
-          <!-- 状態変更ボタンのモック -->
-          <button>{{ item.state }}</button>
-        </td>
-        <td class="button">
-          <!-- 削除ボタンのモック -->
-          <button>削除</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
+        <table>
+            <thead>
+            <tr>
+                <th class="id" scope="col">ID</th>
+                <th class="comment" scope="col">コメント</th>
+                <th class="state" scope="col">状態</th>
+                <th class="button" scope="col">-</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in todos" :key="item.id">
+                <th scope="row">{{ item.id }}</th>
+                <td>{{ item.comment }}</td>
+                <td class="state">
+                    <!-- 状態変更ボタンのモック -->
+                    <button>{{ item.state }}</button>
+                </td>
+                <td class="button">
+                    <!-- 削除ボタンのモック -->
+                    <button>削除</button>
+                </td>
+            </tr>
+            </tbody>
+        </table>
 
-    <h2>新しい作業の追加</h2>
-    <form class="add-form" @submit.prevent="doAdd">
-      <label for="comment">コメント</label>
-      <input id="comment" type="text" ref="comment">
-      <button type="submit">追加</button>
-    </form>
-  </div>
+        <h2>新しい作業の追加</h2>
+        <form class="add-form" @submit.prevent="doAdd">
+            <label for="comment">コメント</label>
+            <input id="comment" type="text" ref="comment">
+            <button type="submit">追加</button>
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {},
-  data() {
-    return {
-      todos: []
-    }
-  },
-  methods: {
-    // ToDo 追加の処理
-    doAdd: function () {
-      // ref で名前を付けておいた要素を参照
-      let comment = this.$refs.comment
-      // 入力がなければ何もしないで return
-      if (!comment.value.length) {
-        return
-      }
-      // { 新しいID, コメント, 作業状態 }というオブジェクトを現在の todos リストへ push
-      // 作業状態「state」はデフォルト「作業中=0」で作成
-      this.todos.push({
-        id: todoStorage.uid++,
-        comment: comment.value,
-        state: 0
-      })
-      // フォーム要素を空にする
-      comment.value = ''
+    name: 'App',
+    components: {},
+    data() {
+        return {
+            todos: []
+        }
     },
-    watch: {
-      // オプションを使う場合はオブジェクト形式にする
-      todos: {
-        // 引数はウォッチしているプロパティの変更後の値
-        handler: function (todos) {
-          todoStorage.save(todos)
+
+    methods: {
+        // ToDo 追加の処理
+        doAdd: function () {
+            // ref で名前を付けておいた要素を参照
+            let comment = this.$refs.comment
+            // 入力がなければ何もしないで return
+            if (!comment.value.length) {
+                return
+            }
+            // { 新しいID, コメント, 作業状態 }というオブジェクトを現在の todos リストへ push
+            // 作業状態「state」はデフォルト「作業中=0」で作成
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+            // フォーム要素を空にする
+            comment.value = ''
         },
-        // deep オプションでネストしているデータも監視できる
-        deep: true
-      }
+
+        watch: {
+            // オプションを使う場合はオブジェクト形式にする
+            todos: {
+                // 引数はウォッチしているプロパティの変更後の値
+                handler: function (todos) {
+                    todoStorage.save(todos)
+                },
+                // deep オプションでネストしているデータも監視できる
+                deep: true
+            }
+        },
+
+        created() {
+            // インスタンス作成時に自動的に fetch() する
+            this.todos = todoStorage.fetch()
+        },
     }
-  }
 }
 
 // @see: https://jp.vuejs.org/v2/examples/todomvc.html
 let STORAGE_KEY = 'todos-vuejs-demo'
 let todoStorage = {
-  fetch: function () {
-    let todos = JSON.parse(
-        localStorage.getItem(STORAGE_KEY) || '[]'
-    )
-    todos.forEach(function (todo, index) {
-      todo.id = index
-    })
-    todoStorage.uid = todos.length
-    return todos
-  },
-  save: function (todos) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
-  }
+    fetch: function () {
+        let todos = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach(function (todo, index) {
+            todo.id = index
+        })
+        todoStorage.uid = todos.length
+        return todos
+    },
+    save: function (todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
 }
 </script>
 
 <style>
 .app__main {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 
-  /* 以上CSSは元々あったもの */
-  /* 以下参考main.cssよりコピペ */
-  /* @see: https://github.com/mio3io/cr-vue/blob/master/codes/tutorial-todo/main.css */
+    /* 以上CSSは元々あったもの */
+    /* 以下参考main.cssよりコピペ */
+    /* @see: https://github.com/mio3io/cr-vue/blob/master/codes/tutorial-todo/main.css */
 
-  max-width: 640px;
-  margin: 0 auto;
+    max-width: 640px;
+    margin: 0 auto;
 }
 
 * {
-  box-sizing: border-box;
+    box-sizing: border-box;
 }
 
 table {
-  width: 100%;
-  border-collapse: collapse;
+    width: 100%;
+    border-collapse: collapse;
 }
 
 thead th {
-  border-bottom: 2px solid #0099e4; /*#d31c4a */
-  color: #0099e4;
+    border-bottom: 2px solid #0099e4; /*#d31c4a */
+    color: #0099e4;
 }
 
 th,
 td {
-  padding: 0 8px;
-  line-height: 40px;
+    padding: 0 8px;
+    line-height: 40px;
 }
 
 thead th.id {
-  width: 50px;
+    width: 50px;
 }
 
 thead th.state {
-  width: 100px;
+    width: 100px;
 }
 
 thead th.button {
-  width: 60px;
+    width: 60px;
 }
 
 tbody td.button, tbody td.state {
-  text-align: center;
+    text-align: center;
 }
 
 tbody tr td,
 tbody tr th {
-  border-bottom: 1px solid #ccc;
-  transition: all 0.4s;
+    border-bottom: 1px solid #ccc;
+    transition: all 0.4s;
 }
 
 tbody tr.done td,
 tbody tr.done th {
-  background: #f8f8f8;
-  color: #bbb;
+    background: #f8f8f8;
+    color: #bbb;
 }
 
 tbody tr:hover td,
 tbody tr:hover th {
-  background: #f4fbff;
+    background: #f4fbff;
 }
 
 button {
-  border: none;
-  border-radius: 20px;
-  line-height: 24px;
-  padding: 0 8px;
-  background: #0099e4;
-  color: #fff;
-  cursor: pointer;
+    border: none;
+    border-radius: 20px;
+    line-height: 24px;
+    padding: 0 8px;
+    background: #0099e4;
+    color: #fff;
+    cursor: pointer;
 }
 </style>
