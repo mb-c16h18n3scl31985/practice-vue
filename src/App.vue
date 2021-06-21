d9f65c1a34ef3c934621f0904eab72aa5e9a5810
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-06-21 16:29:27 +0900

    Add: ストレージにデータを保存する

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,23 @@ export default {
   }
 }
 
-
+// @see: https://jp.vuejs.org/v2/examples/todomvc.html
+let STORAGE_KEY = 'todos-vuejs-demo'
+let todoStorage = {
+  fetch: function() {
+    let todos = JSON.parse(
+        localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
 </script>
 
 <style>
