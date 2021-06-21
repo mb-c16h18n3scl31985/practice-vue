aafab60767e30168fef48a761514a8052e16baf1
yumi takahashi <c16h18n3scl31985@gmail.com>
2021-06-21 15:34:24 +0900

    Add: CSSの追加(コピペ)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id='app'>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -14,6 +14,8 @@ export default {
     HelloWorld
   }
 }
+
+
 </script>
 
 <style>
@@ -23,6 +25,74 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  /* 以上CSSは元々あったもの */
+  /* 以下参考main.cssよりコピペ */
+  /*　@see: https://github.com/mio3io/cr-vue/blob/master/codes/tutorial-todo/main.css　*/
+  max-width: 640px;
+  margin: 0 auto;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+thead th {
+  border-bottom: 2px solid #0099e4; /*#d31c4a */
+  color: #0099e4;
+}
+
+th,
+td {
+  padding: 0 8px;
+  line-height: 40px;
+}
+
+thead th.id {
+  width: 50px;
+}
+
+thead th.state {
+  width: 100px;
+}
+
+thead th.button {
+  width: 60px;
+}
+
+tbody td.button, tbody td.state {
+  text-align: center;
+}
+
+tbody tr td,
+tbody tr th {
+  border-bottom: 1px solid #ccc;
+  transition: all 0.4s;
+}
+
+tbody tr.done td,
+tbody tr.done th {
+  background: #f8f8f8;
+  color: #bbb;
+}
+
+tbody tr:hover td,
+tbody tr:hover th {
+  background: #f4fbff;
+}
+
+button {
+  border: none;
+  border-radius: 20px;
+  line-height: 24px;
+  padding: 0 8px;
+  background: #0099e4;
+  color: #fff;
+  cursor: pointer;
 }
 </style>
